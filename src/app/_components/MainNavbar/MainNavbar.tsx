0b81e764a03664627e4fb96c61e1b68227f1228a
Raphael Americano <raphaelamericano@Raphaels-MacBook-Pro.local>
2024-01-18 00:49:21 -0300

    fix menu

--- src/app/_components/MainNavbar/MainNavbar.tsx
+++ src/app/_components/MainNavbar/MainNavbar.tsx
@@ -1,12 +1,18 @@
+import { getMenu } from "@/app/actions/get-menu";
 import MainNavbarBrand from "./MainNavbarBrand";
-import MainNavbarItems from "./MainNavbarItems";
-import { Navbar } from "@nextui-org/react";
+
+import { Navbar, NavbarContent } from "@nextui-org/react";
+import MainNavbarItem from "./MainNavbarItem";
 
 export default async function MainNavbar() {
+  const menu = await getMenu("navbarMenu")
+   
   return (
     <Navbar position="static">
       <MainNavbarBrand />
-      <MainNavbarItems />
+      <NavbarContent justify="end">
+        {menu.map(({ id, label }) => <MainNavbarItem key={id} title={label} /> )}
+      </NavbarContent>
     </Navbar>
   );
 }
--- src/app/actions/get-menu.ts
+++ src/app/actions/get-menu.ts
@@ -1,6 +1,6 @@
 "use server"
 import { getMenuByName } from "@/services/graphql/menu"
-import MainNavbarItem from "./../_components/MainNavbar/MainNavbarItems/MainNavbarItem"
+import MainNavbarItem from "../_components/MainNavbar/MainNavbarItem"
 export const getMenu = async (name:string) => {
   const result = await getMenuByName(name)
   const { menu:{ menuItems: { edges }} } = result;
